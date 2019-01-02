ffba5574f4bdb5cc55c560f491f7086d855b5955
chengxiang <chengxiang085@gmail.com>
2019-01-02 16:48:01 +0800

    modify the structure of NoteServer

--- NoteServer/server/noteServer.cpp
+++ NoteServer/server/noteServer.cpp
@@ -1,5 +1,33 @@
-/*noteServer V0.3
- *using IO multiplexing, implement a simple server
+/*
+* Copyright (c) 2018, Leonardo Cheng <chengxiang085@gmail.com>.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+*  1. Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*
+*  2. Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*@file noteServer.cpp
+ *@brief using IO multiplexing, implement a simple server
 */
 
 #include "noteServer.h"
@@ -7,6 +35,7 @@
 #include "sqlExecuteResult.h"
 #include "sqlExecuteResultFactory.h"
 #include <sstream>
+#include <memory>
 using namespace std;
 
 int getResultFromMysql(const string &request, string &response);
@@ -139,7 +168,9 @@ int getResultFromMysql(const string &request, string &response)
 	}
 
 	//string result;
-	CSqlExecuteResult* pSqlExecute = CSqlExecuteResultFactory::create(vectQueryElement[0]);
+	//CSqlExecuteResult* pSqlExecute = CSqlExecuteResultFactory::create(vectQueryElement[0]);
+	shared_ptr<CSqlExecuteResult> pSqlExecute(CSqlExecuteResultFactory::create(vectQueryElement[0]));
+
 	pSqlExecute->assginParameter(vectQueryElement);
 	if(pSqlExecute->executeAndGetResult(response) < 0)
 	{
--- NoteServer_V0.3/include/noteServer.h
+++
@@ -1,7 +0,0 @@
-#ifndef _NOTE_SERVER_H_
-#define _NOTE_SERVER_H_
-
-#include "noteHeader.h"
-#include "sqlExecuteResult.h"
-
-#endif
